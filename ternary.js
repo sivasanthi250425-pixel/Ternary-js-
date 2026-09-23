9666be90995b7d6b14ef836ed79a08ddcb92db67
SIVASANTHI R <sivasanthi.250425@cse.ritchennai.edu.in>
2026-09-24 00:59:33 +0530

    click here

---
+++ ternary.js
@@ -0,0 +1,8 @@
+const practisetimemins=20
+const msg=practisetimemins<30 ? "you nee to try harder":practisetimemins<80 ? "doing good":"excell3ent"
+console.log(msg)
+
+const playerguess=9
+const correctans=9
+const msg2=playerguess>correctans? "too high":playerguess<correctans? "too low" :"correct"
+console.log(msg2)
